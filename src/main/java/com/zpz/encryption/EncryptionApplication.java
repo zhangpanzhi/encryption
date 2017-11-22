d4476cf5711a27ba1519b43c4379a2eabdb511da
zhangpanzhi <zhangpanzhi@rongcapital.cn>
2017-11-22 10:51:22 +0800

    初始化项目

---
+++ src/main/java/com/zpz/encryption/EncryptionApplication.java
@@ -0,0 +1,12 @@
+package com.zpz.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionApplication.class, args);
+	}
+}
